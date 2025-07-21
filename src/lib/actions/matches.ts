7777adbe32bc47c0ce6c815ae10e2a442076940e
Alvaro Marina <alvarommarina14@hotmail.com>
2025-07-21 10:34:31 -0300

    refactor: removed api routes and moved them to server actions

--- src/lib/actions/matches.ts
+++ src/lib/actions/matches.ts
@@ -1,46 +1,34 @@
+'use server';
+import { MatchCreateInputType, MatchUpdateInputType, MatchUpdateInputWithIdType } from '@/types/matches';
 import {
-    MatchCreateInputType,
-    MatchUpdateInputType,
-    MatchUpdateInputWithIdType,
-} from '@/types/matches';
+    updateMatchBulkService,
+    createMatchBulkService,
+    createMatchService,
+    deleteMatchBulkService,
+    updateMatchService,
+    deleteMatchService,
+} from '@/lib/services/matches';
 
 export async function createMatch(data: MatchCreateInputType) {
-    return await fetch('api/matches', {
-        method: 'POST',
-        body: JSON.stringify(data),
-    });
+    return await createMatchService(data);
 }
 
 export async function createMatchBulk(data: MatchCreateInputType[]) {
-    return await fetch('api/matches', {
-        method: 'POST',
-        body: JSON.stringify(data),
-    });
+    return await createMatchBulkService(data);
 }
 
 export async function updateMatch(id: number, data: MatchUpdateInputType) {
-    return await fetch(`api/matches/'${id}`, {
-        method: 'PUT',
-        body: JSON.stringify(data),
-    });
+    return await updateMatchService(Number(id), data);
 }
 
 export async function updateMatchBulk(data: MatchUpdateInputWithIdType[]) {
-    return await fetch('api/matches', {
-        method: 'PUT',
-        body: JSON.stringify(data),
-    });
+    return await updateMatchBulkService(data);
 }
 
 export async function deleteMatch(id: number) {
-    return await fetch(`api/matches/${id}`, {
-        method: 'DELETE',
-    });
+    return await deleteMatchService(Number(id));
 }
 
 export async function deleteMatchBulk(ids: number[]) {
-    return await fetch('api/matches', {
-        method: 'DELETE',
-        body: JSON.stringify({ ids }),
-    });
+    return await deleteMatchBulkService(ids);
 }
