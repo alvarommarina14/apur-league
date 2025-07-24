4889abfffabacf146b52ee62620020937eef54cd
Alvaro Marina <alvarommarina14@hotmail.com>
2025-07-24 17:23:13 -0300

    feat: test revalidate path

--- src/lib/actions/matchWeek.ts
+++ src/lib/actions/matchWeek.ts
@@ -1,9 +1,11 @@
 'use server';
 import { getMatchWeekWithMatches } from '@/lib/services/matchWeek';
 import { createMatchWeek } from '@/lib/services/matchWeek';
+import { revalidatePath } from 'next/cache';
 
 export async function createMatchWeekAction() {
     try {
+        revalidatePath('/admin/matchWeeks');
         return await createMatchWeek();
     } catch {
         throw new Error('No se pudo crear la fecha');
